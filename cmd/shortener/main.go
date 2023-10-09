61bee13b8be057083066a8647e2a68c891edf4f8
nartim-win <hissan2007@gmail.com>
2023-10-09 20:40:08 +0300

    sec handler

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,51 +1,89 @@
 package main
 
 import (
+	"io"
 	"log"
+	"math/rand"
 	"net/http"
+	"strings"
+	"time"
 )
 
-var SavedData map[string]string
+var URLs = make(map[string]string)
 
-type CustURL struct {
-	full    string
-	shorten string
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+const host = "localhost"
+const port = "8080"
+const schema = "http"
+const shortURLLen = 8
+
+type fullURL struct {
+	name string
 }
+type shortURL string
 
 type Repository interface {
-	Save() bool
-	IsExist() bool
+	// Save сохранение данных.
+	Save(storage any) shortURL
+
+	// IsExist проверка есть ли короткий урл в базе.
+	// True если есть, false - нет.
+	IsExist(shortUrl string) bool
 }
 
-func (url *CustURL) Save() bool {
-	if url.IsExist() {
+func (url *fullURL) Save(storage any) shortURL {
+	sUrl := generateShortUrl(shortURLLen)
+	s := storage.(map[string]string)
+	s[sUrl] = url.name
+	return shortURL(sUrl)
+}
+
+func (url *fullURL) IsExist(shortUrl string) bool {
+	if _, ok := URLs[shortUrl]; !ok {
 		return false
 	}
-	SavedData[url.shorten] = url.full
 	return true
 }
 
-func (url *CustURL) IsExist() bool {
-	if _, ok := SavedData[url.shorten]; !ok {
-		return false
+// generateShortUrl возвращает строку из случайных символов.
+func generateShortUrl(n int64) string {
+	rand.New(rand.NewSource(time.Now().UnixNano()))
+	shortKey := make([]byte, n)
+	for i := range shortKey {
+		shortKey[i] = charset[rand.Intn(len(charset))]
 	}
-	return true
-}
-
-var allowedMethods = map[string]string{
-	"GET":  http.MethodGet,
-	"POST": http.MethodPost,
+	return string(shortKey)
 }
 
 func mainPage(w http.ResponseWriter, r *http.Request) {
-	meth, ok := allowedMethods[r.Method]
-	if !ok {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		_, _ = w.Write([]byte("Method not allowed."))
+	if err := r.ParseForm(); err != nil {
+		_, _ = w.Write([]byte(err.Error()))
+		return
 	}
-	if meth == http.MethodPost {
-		w.Header().Set("Content-Type", "text/plain")
+	body, _ := io.ReadAll(r.Body)
 
+	if r.Method == http.MethodPost {
+		fURL := fullURL{string(body)}
+		sURL := fURL.Save(URLs)
+		result := schema + "://" + host + ":" + port + "/" + sURL
+
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusCreated)
+
+		_, _ = w.Write([]byte(result))
+
+	} else if r.Method == http.MethodGet {
+		path := r.URL.Path
+		id := strings.Split(path, "/")[1]
+
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusTemporaryRedirect)
+
+		_, _ = w.Write([]byte(id))
+
+	} else {
+		w.WriteHeader(http.StatusBadRequest)
+		return
 	}
 }
 

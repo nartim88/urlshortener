6aa0c7d7e5e7822cd63718f779d310ae19a33176
Nikita Artimovich <hissan2007@gmail.com>
2023-10-17 15:27:08 +0300

    Revert "flags bugfix"
    This reverts commit ebefa1fca9ae4484a1f949ef525d7ebdd37d0ab7.

--- cmd/shortener/config/flags.go
+++ cmd/shortener/config/flags.go
@@ -10,6 +10,4 @@ var (
 func parseFlags() {
 	flag.StringVar(&FlagRunAddr, "a", "localhost:8080", "address and port to run server")
 	flag.StringVar(&FlagBaseAddr, "b", "http://localhost:8080", "server address before shorten URL")
-
-	flag.Parse()
 }

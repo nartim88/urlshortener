e3bc8fd9b3dadf263f8737f2926f0ae7a2c942fd
Nikita Artimovich <hissan2007@gmail.com>
2023-12-05 16:55:07 +0300

    minor

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"github.com/nartim88/urlshortener/config"
 	"github.com/nartim88/urlshortener/internal/app/shortener"
+	"github.com/nartim88/urlshortener/pkg/logger"
 )
 
 func main() {
 	cfg := config.NewConfig()
 	err := cfg.ParseConfigs()
 	if err != nil {
+		logger.Log.Error().Stack().Err(err).Send()
 		panic(err)
 	}
 	app := shortener.NewApp()

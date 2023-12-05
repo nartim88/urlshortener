c35ff14e1de0dfad05b96b802634832e63dcf3cf
Nikita Artimovich <hissan2007@gmail.com>
2023-12-05 16:21:03 +0300

    logger is added to config.go

--- config/config.go
+++ config/config.go
@@ -2,9 +2,11 @@ package config
 
 import (
 	"flag"
+	"os"
 
 	"github.com/caarlos0/env"
 	"github.com/joho/godotenv"
+	"github.com/rs/zerolog"
 )
 
 type Config struct {
@@ -18,20 +20,16 @@ type Config struct {
 
 // NewConfig инициализирует Config с дефолтными значениями
 func NewConfig() *Config {
-	cfg := Config{
-		RunAddr:  RunAddr,
-		BaseURL:  BaseURL,
-		LogLevel: LogLevel,
-	}
-	return &cfg
+	return &Config{}
 }
 
 // ParseConfigs инициализация парсинга конфигов из окружения и флагов
 func (conf *Config) ParseConfigs() error {
+	logger := zerolog.New(os.Stdout)
 	if err := conf.parseDotenv(); err != nil {
 		return err
 	}
-	conf.parseFlags()
+	conf.parseFlags(logger)
 	if err := conf.parseEnv(); err != nil {
 		return err
 	}
@@ -48,14 +46,27 @@ func (conf *Config) parseEnv() error {
 }
 
 // parseFlags парсит флаги командной строки
-func (conf *Config) parseFlags() {
-	flag.StringVar(&conf.RunAddr, "a", RunAddr, "address and port to run server")
-	flag.StringVar(&conf.BaseURL, "b", BaseURL, "server address before shorten URL")
+func (conf *Config) parseFlags(logger zerolog.Logger) {
+	flag.StringVar(&conf.RunAddr, "a", "", "address and port to run server")
+	flag.StringVar(&conf.BaseURL, "b", "", "server address before shorten URL")
 	flag.StringVar(&conf.LogLevel, "l", LogLevel, "log level")
 	flag.StringVar(&conf.FileStoragePath, "f", "", "full file name for saving URLs")
 	flag.StringVar(&conf.DatabaseDSN, "d", "", "database DSN")
-
 	flag.Parse()
+
+	logger.Info().
+		Str("SERVER_ADDRESS", conf.RunAddr).
+		Str("BASE_URL", conf.BaseURL).
+		Str("LOG_LEVEL", conf.LogLevel).
+		Str("FILE_STORAGE_PATH", conf.FileStoragePath).
+		Str("DATABASE_DSN", conf.DatabaseDSN).
+		Str("SECRET_KEY", func() string {
+			if conf.SecretKey != "" {
+				return "true"
+			}
+			return "false"
+		}()).
+		Msg("got flags:")
 }
 
 // parseDotenv загружает в окружение переменные из .env
